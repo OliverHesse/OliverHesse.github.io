bdf64cb06e0714adf482a2d85dceda2de4be895e
lunarLucent <109795653+lunarLucent@users.noreply.github.com>
2024-07-02 10:35:32 +0100

    should be finished

--- mypage/src/App.js
+++ mypage/src/App.js
@@ -1,58 +1,85 @@
 import logo from './logo.svg';
+
+import cliTextEditorGif from './projectgif/CLITextEditorGif.gif';
+import basicInterpreterGif from './projectgif/BasicInterpreterGif.gif';
+import curve from "./projectgif/CurveGif.gif"
+import financeApp from "./projectgif/ChildFinanceAppGif.gif"
 import './App.css';
 import React , { useState }from 'react';
 import ReactDOM from 'react-dom/client';
+import { AnimatePresence, motion } from "framer-motion"
 
 
-
-function ProjectCard({orientation,size,project,changeFocused,focused,animClass,clickHandler}){
+function ProjectCard({orientation,size,project,classes,clickHandler}){
  
-  return (<div className={'ProjectCard ' + orientation + " "+ animClass} onClick={()=>clickHandler(orientation)}>
-        <p>Test</p>
-        <p className='paragraph'>{project["name"]}</p>
-        <p className='paragraph'>{project["Image"]}</p>
+  return (<motion.div className={'ProjectCard ' +classes} 
+    onClick={()=>clickHandler(orientation)}
+  
+    >
+       <img src={project["Image"]}className='projectImage'></img>
+       <p className='projectTitle'>{project["name"]}</p>
 
-    </div>);
+    </motion.div>);
 }
+function FocusedProjectCard({project,clickHandler}){
 
+  return(
+    <motion.div 
+      className={"ProjectCard center"}
+      whileHover={{scale:1.2}}
+      >
+        <a href={project["link"]}>
+          <img src={project["Image"]}className='projectImage'></img>
+          <p className='projectTitle'>{project["name"]}</p>
+        </a>
+     
+
+    </motion.div>
+  )
+}
 function ProjectView(){
   const [focused,setFocused] = useState(0);
   const [leftAnim, setLeftAnim] = useState('');
   const [centerAnim, setCenterAnim] = useState('');
   const [rightAnim, setRightAnim] = useState('');
+  const [reset, setReset] = useState(false);
   let Projects = [
-    {"name":"Project 1","Image":"fake"},
-    {"name":"Project 2","Image":"fake"},
-    {"name":"Project 3","Image":"fake"},
-    {"name":"Project 4","Image":"fake"},
+    {"name":"CLI Text Editor","Image":cliTextEditorGif,"link":"https://github.com/OliverHesse/CLI-Text-Editor"},
+    {"name":"Basic Interpreter","Image":basicInterpreterGif,"link":"https://github.com/OliverHesse/CustomInterpreter"},
+    {"name":"Bézier curve","Image":curve,"link":"https://github.com/OliverHesse/B-zier-curve"},
+    {"name":"Child Finance App","Image":financeApp,"link":"https://github.com/OliverHesse/Child-Finance-App"},
     {"name":"Project 5","Image":"fake"},
     {"name":"Project 6","Image":"fake"}
   ]
   let size = 6;
+  let resetAnimations = false;
   const clickHandler =  (cardOrientation) =>{
+    setReset(false);
     if(cardOrientation =="left"){
       let result = (focused - 1 + size) % size;
-      setLeftAnim("leftLeaveFrame")
-      setCenterAnim("centerShuffleLeft")
-      setRightAnim("rightShuffleCenter")
+      setLeftAnim("left")
+      setCenterAnim("left")
+      setRightAnim("center")
       
       setFocused(result);
     }else if(cardOrientation == "right"){
       let result = (focused + 1 + size) % size;
-      setLeftAnim("leftShuffleCenter")
-      setCenterAnim("centerShuffleRight")
-      setRightAnim("rightLeaveFrame")
+      setLeftAnim("center")
+      setCenterAnim("right")
+      setRightAnim("right")
       setFocused(result);
+
     }
   };
-
 
   let left = (focused - 1 + size) % size;
   let right = (focused + 1 + size) % size;
   return(<div className='ProjectDisplay'>
-      <ProjectCard orientation={"left"} size={size} project = {Projects[left]} changeFocused ={setFocused} focused={focused} clickHandler={clickHandler} animClass={leftAnim}/>
-      <ProjectCard orientation={"center"} size={size} project = {Projects[focused]} changeFocused ={setFocused}focused={focused} clickHandler={clickHandler}animClass={centerAnim}/>
-      <ProjectCard orientation={"right"} size={size} project = {Projects[right]}changeFocused ={setFocused}focused={focused} clickHandler={clickHandler}animClass={rightAnim}/>
+    
+      <ProjectCard orientation={"left"} size={size} project = {Projects[left]} classes={"left"} clickHandler={clickHandler} />
+      <FocusedProjectCard project = {Projects[focused] } animateDirection={centerAnim}  />
+      <ProjectCard orientation={"right"} size={size} project = {Projects[right]} classes={"right"}  clickHandler={clickHandler} />
+      
     </div>
   )
 }
